94dcc98302ba62903d94153d649df7e4371e5642
Raymond <akdfhr2@gmail.com>
2024-06-18 11:14:51 +0900

    test: 환경변수 변경

--- src/shared/modules/config.module.ts
+++ src/shared/modules/config.module.ts
@@ -8,7 +8,7 @@ import { Env } from '@/shared/interfaces/env.interface'
   imports: [
     BaseConfigModule.forRoot({
       isGlobal: true,
-      envFilePath:`.env.${process.env.NODE_ENV}` ,
+      envFilePath:process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env",
       validationSchema: Joi.object<Required<Env>>({
         NODE_ENV: Joi.valid(
           ...['production', 'development', 'test'],
