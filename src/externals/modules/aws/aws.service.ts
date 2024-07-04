73fd7aa9f28aabfbec16d242f21001f68eeda948
Raymond <akdfhr2@gmail.com>
2024-07-05 01:49:49 +0900

    fix: 버킷 변경

--- src/externals/modules/aws/aws.service.ts
+++ src/externals/modules/aws/aws.service.ts
@@ -5,6 +5,7 @@ import { MemoryStoredFile } from 'nestjs-form-data'
 import { v4 as uuid } from 'uuid'
 import { ConfigService } from '@nestjs/config'
 import { Env } from '@/shared/interfaces/env.interface'
+import { ApiException } from '@/shared/exceptions/exception.interface'
 
 export class AwsService {
   constructor(
@@ -13,24 +14,28 @@ export class AwsService {
   ) {}
 
   async uploadFiles(files: MemoryStoredFile[], folder: string = '') {
-    const uploadUrls = await Promise.all(
-      files.map(
-        (file) =>
-          new Promise<string>(async (resolve) => {
-            const key = folder + '/' + uuid()
-            const command = new PutObjectCommand({
-              Key: key,
-              Body: file.buffer,
-              Bucket: 'poop-api',
-              ACL: ObjectCannedACL.public_read,
-            })
-            await this.s3Client.send(command)
-            resolve(
-              `https://${this.configService.get('AWS_S3_BUCKET_NAME')}.s3.${this.configService.get<string>('AWS_S3_REGION')}.amazonaws.com/${key}`,
-            )
-          }),
-      ),
-    )
-    return uploadUrls
+    try {
+      const uploadUrls = await Promise.all(
+        files.map(
+          (file) =>
+            new Promise<string>(async (resolve) => {
+              const key = folder + '/' + uuid()
+              const command = new PutObjectCommand({
+                Key: key,
+                Body: file.buffer,
+                Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
+                ACL: ObjectCannedACL.public_read,
+              })
+              await this.s3Client.send(command)
+              resolve(
+                `https://${this.configService.get('AWS_S3_BUCKET_NAME')}.s3.${this.configService.get<string>('AWS_S3_REGION')}.amazonaws.com/${key}`,
+              )
+            }),
+        ),
+      )
+      return uploadUrls
+    } catch (err) {
+      throw ApiException.PLAIN_BAD_REQUEST()
+    }
   }
 }
